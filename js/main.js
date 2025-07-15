c33b6ca4ce74ae75acff7b55f6d81d4f4b63d3fb
islammosmar1 <124386471+islammosmar1@users.noreply.github.com>
2025-07-15 17:07:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Function to initialize carousels
+    window.initializeCarousels = function() { // Make it global for language.js to access
+        // Header carousel
+        $(".header-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1500,
+            items: 1,
+            dots: true,
+            loop: true,
+            nav : true,
+            navText : [
+                '<i class="bi bi-chevron-left"></i>',
+                '<i class="bi bi-chevron-right"></i>'
+            ],
+            rtl: (document.documentElement.getAttribute('dir') === 'rtl') // Set RTL based on current document direction
+        });
+
+
+        // Testimonials carousel
+        $(".testimonial-carousel").owlCarousel({
+            autoplay: true,
+            smartSpeed: 1000,
+            margin: 24,
+            dots: false,
+            loop: true,
+            nav : true,
+            navText : [
+                '<i class="bi bi-arrow-left"></i>',
+                '<i class="bi bi-arrow-right"></i>'
+            ],
+            rtl: (document.documentElement.getAttribute('dir') === 'rtl') // Set RTL based on current document direction
+        });
+    };
+
+    // No direct carousel initialization here. It will be initialized by language.js
+    // or by calling initializeCarousels() on page load if needed directly.
+
+})(jQuery);
